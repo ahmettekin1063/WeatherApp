ecc7c03478d86e8f20f65c7dc371a35302f1189a
Ahmet Tekin <ahmedtekin1063@hotmail.com>
2020-11-11 13:27:30 +0300

    project updated

--- app/src/main/java/com/ahmettekin/retrofitjava/view/MainActivity.java
+++ app/src/main/java/com/ahmettekin/retrofitjava/view/MainActivity.java
@@ -1,10 +1,13 @@
 package com.ahmettekin.retrofitjava.view;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.database.CrossProcessCursor;
 import android.os.Bundle;
 
 import com.ahmettekin.retrofitjava.R;
+import com.ahmettekin.retrofitjava.adapter.RecyclerViewAdapter;
 import com.ahmettekin.retrofitjava.model.CryptoModel;
 import com.ahmettekin.retrofitjava.service.CryptoAPI;
 import com.google.gson.Gson;
@@ -13,10 +16,14 @@ import com.google.gson.GsonBuilder;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,47 +31,89 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<CryptoModel> cryptoModels;
     private String BASE_URL = "https://api.nomics.com/v1/";
     Retrofit retrofit;
+    RecyclerView recyclerView;
+    RecyclerViewAdapter recyclerViewAdapter;
+
+    CompositeDisposable compositeDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //https://api.nomics.com/v1/prices?key=d58e4dc4e10e1b73538d6d41f61ba0b1
+        //https://api.nomics.com/v1/prices?key=2187154b76945f2373394aa34f7dc98a
+        //https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json
 
+        recyclerView = findViewById(R.id.recyclerView);
 
-      /*  Gson gson = new GsonBuilder().setLenient().create();
+        //Retrofit & JSON
+
+        Gson gson = new GsonBuilder().setLenient().create();
 
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
 
+
         loadData();
+
     }
+
     private void loadData(){
 
-        CryptoAPI cryptoAPI = retrofit.create(CryptoAPI.class);
+        final CryptoAPI cryptoAPI = retrofit.create(CryptoAPI.class);
 
+        compositeDisposable = new CompositeDisposable();
+
+        compositeDisposable.add(cryptoAPI.getData()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::handleResponse));
+
+        /*
         Call<List<CryptoModel>> call = cryptoAPI.getData();
-
         call.enqueue(new Callback<List<CryptoModel>>() {
             @Override
             public void onResponse(Call<List<CryptoModel>> call, Response<List<CryptoModel>> response) {
-                if(response.isSuccessful()){
+                if (response.isSuccessful()) {
                     List<CryptoModel> responseList = response.body();
                     cryptoModels = new ArrayList<>(responseList);
-                    for ( CryptoModel cryptoModel : cryptoModels){
+                    //RecyclerView
+                    recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                    recyclerViewAdapter = new RecyclerViewAdapter(cryptoModels);
+                    recyclerView.setAdapter(recyclerViewAdapter);
+                    for (CryptoModel cryptoModel : cryptoModels) {
                         System.out.println(cryptoModel.currency);
                         System.out.println(cryptoModel.price);
-                    }*/
+                    }
                 }
             }
-
             @Override
             public void onFailure(Call<List<CryptoModel>> call, Throwable t) {
                 t.printStackTrace();
             }
         });
+*/
+
+    }
+
+
+    private void handleResponse(List<CryptoModel> cryptoModelList) {
+        cryptoModels = new ArrayList<>(cryptoModelList);
+
+        //RecyclerView
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        recyclerViewAdapter = new RecyclerViewAdapter(cryptoModels);
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        compositeDisposable.clear();
     }
 }
