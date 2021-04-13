e1eef0d037594679891de2c14c268cfddc7cf092
ahmettekin1063 <ahmedtekin1063@hotmail.com>
2021-04-13 17:54:51 +0300

    some developments

--- app/src/main/java/com/ahmettekin/weatherappkotlin/service/WeatherApiClient.kt
+++ app/src/main/java/com/ahmettekin/weatherappkotlin/service/WeatherApiClient.kt
@@ -1,22 +1,30 @@
 package com.ahmettekin.weatherappkotlin.service
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 object WeatherApiClient {
 
-    val BASE_URL="http://api.openweathermap.org/data/2.5/"
+    val BASE_URL="https://api.openweathermap.org/data/2.5/"
     private var retrofit: Retrofit? = null
     var gson = GsonBuilder().setLenient().create()
+    val clientBuilder = OkHttpClient.Builder()
+    val loggingInterceptor= HttpLoggingInterceptor()
+
     val client: Retrofit?
         get() {
             if(retrofit ==null){
+                loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+                clientBuilder.addInterceptor(loggingInterceptor)
                 retrofit =Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .client(clientBuilder.build())
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create(gson))
                     .build()
             }
             return retrofit
--- app/src/main/java/com/ahmettekin/weatherappkotlin/view/ListFragment.kt
+++ app/src/main/java/com/ahmettekin/weatherappkotlin/view/ListFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ahmettekin.weatherappkotlin.R
+import com.ahmettekin.weatherappkotlin.adapter.RecyclerViewAdapter
 import com.ahmettekin.weatherappkotlin.model.WeatherModel
 import com.ahmettekin.weatherappkotlin.service.WeatherApiClient
 import com.ahmettekin.weatherappkotlin.service.WeatherApiInterface
@@ -13,9 +15,11 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableObserver
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
 
+    lateinit var compositeDisposable: CompositeDisposable
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
@@ -23,31 +27,62 @@ class ListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         loadData()
-
     }
+
+    /*private fun loadData() {
+        WeatherApiClient.client?.let {
+            val weatherApi = it.create(WeatherApiInterface::class.java)
+            val compositeDisposable = CompositeDisposable()
+            weatherApi.getWeatherData("5174095,1850147,2013159")
+                ?.subscribeOn(Schedulers.io())
+                ?.observeOn(AndroidSchedulers.mainThread())?.let { observable ->
+                    compositeDisposable.add(
+                        observable
+                            .subscribeWith(object : DisposableObserver<WeatherModel>() {
+                                override fun onNext(weatherModel: WeatherModel) {
+                                    handleResponse(weatherModel)
+                                }
+
+                                override fun onError(e: Throwable) {
+                                    println("hata:${e.localizedMessage}")
+                                }
+
+                                override fun onComplete() {}
+                            })
+                    )
+                }
+        }
+    }*/
 
     private fun loadData(){
-        val weatherApi=WeatherApiClient.client!!.create(WeatherApiInterface::class.java)
-        val compositeDisposable=CompositeDisposable();
-        compositeDisposable.add(
-            weatherApi.getWeatherData("5174095,1850147,2013159")!!
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(object :DisposableObserver<WeatherModel>(){
-                    override fun onNext(weatherModel: WeatherModel) {
-                        handleResponse(weatherModel)
-                    }
+        val weatherAPI = WeatherApiClient.client?.create(WeatherApiInterface::class.java)
+        compositeDisposable=CompositeDisposable()
+        weatherAPI?.getWeatherData("5174095,1850147,2013159")
+            ?.subscribeOn(Schedulers.io())
+            ?.observeOn(AndroidSchedulers.mainThread())
+            ?.subscribeWith(object : DisposableObserver<WeatherModel>() {
+                override fun onNext(t: WeatherModel) {
+                    handleResponse(t)
+                }
 
-                    override fun onError(e: Throwable) {
-                        TODO("Not yet implemented")
-                    }
+                override fun onError(e: Throwable) {
+                    println("HATA ÇIKTI -> ${e}")
+                }
 
-                    override fun onComplete(){}
-                })
-        )
+                override fun onComplete() {}
+            })?.let {
+                compositeDisposable.add(it)
+            }
     }
 
-    private fun handleResponse(weatherModel: WeatherModel){
-        TODO("RecyclerView öğeleri burada yapılacak")
+    private fun handleResponse(weatherModel: WeatherModel) {
+        recyclerView.layoutManager=LinearLayoutManager(context)
+        recyclerView.adapter= weatherModel.list?.let { RecyclerViewAdapter(it) }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
+    }
+
 }
